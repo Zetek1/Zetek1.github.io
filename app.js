6d72313d35577d1b3c205036f28d0deb5177e327
Zetek1 <106333701+Zetek1@users.noreply.github.com>
2022-11-16 10:07:39 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+let cases = [...document.getElementsByClassName('case')];
+let joueur = document.getElementById('joueur');
+let score1 = document.getElementById('score1');
+let score2 = document.getElementById('score2');
+let scorenul = document.getElementById('scoreNul');
+
+
+let state = {
+    joueurEnCours: 1, 
+    scoreJ1: 0,
+    scoreJ2: 0, 
+    matchNuls: 0, 
+    c1: 0, 
+    c2: 0, 
+    c3: 0, 
+    c4: 0, 
+    c5: 0, 
+    c6: 0, 
+    c7: 0, 
+    c8: 0, 
+    c9: 0, 
+};
+
+cases.forEach((el) =>
+
+)
+
+
